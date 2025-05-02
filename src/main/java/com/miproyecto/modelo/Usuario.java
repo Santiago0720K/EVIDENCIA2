93c3fdf3b54897a7dceddd64811df8d2defdc1f9
jaider santiago <pjaidersantiago@gmail.com>
2025-05-02 09:20:18 -0500

    Se agrago el codigo de el archivo usuario.java

--- src/main/java/com/miproyecto/modelo/Usuario.java
+++ src/main/java/com/miproyecto/modelo/Usuario.java
@@ -4,10 +4,60 @@
  */
 package com.miproyecto.modelo;
 
-/**
- *
- * @author SENA
- */
 public class Usuario {
+    // Atributos del usuario
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
     
+    // Constructor
+    public Usuario(String nombre, String apellido, String email, String password) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.password = password;
+    }
+    
+    // Constructor vacío
+    public Usuario() {
+    }
+    
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getApellido() {
+        return apellido;
+    }
+    
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + '}';
+    }
 }
